053ec902c30a387e648d2956e6492312969aff75
tagoldstein <ticktock01@gmail.com>
2017-06-05 22:07:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,19 +19,19 @@ var STOP_MESSAGE = "Goodbye!";
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
 //=========================================================================================================================================
 var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "Terry is 68 years old.",
+    "Terry has a younger sister who is ten and a half years younger than him.",
+    "Terry has an older brother that is two and a half years older than him.",
+    "Terry will be married for 50 years next month.",
+    "Terry lived in the Brownsville section of Brooklyn until he was 8 years old.",
+    "Terry grew up in Coney Island.",
+    "Terry has two sons and six grandchildren.",
+    "Terry served in the United States Navy in the 1960s.",
+    "Terry has a rare but benign condition called Mirror Dystonia.",
+    "Terry has two different color eyes.",
+    "Terry is a terrible clarinet player.",
+    "Terry has a resting heart beat of only 53 beats per minute.",
+    "Terry has lived in New Jersey since 1978."
 ];
 
 //=========================================================================================================================================
